2d5e256763691f4548b3bf829902b7a02252c0be
Anibal Amoroso <anibaldaniel2212@gmail.com>
2022-06-17 01:03:31 -0300

    implement functionality

--- app/controllers/public_recipes_controller.rb
+++ app/controllers/public_recipes_controller.rb
@@ -1,5 +1,5 @@
 class PublicRecipesController < ApplicationController
-    def index
-      @recipes = Recipe.where(public: true).includes(:user, :recipe_foods, recipe_foods: [:food])
-    end
+  def index
+    @recipes = Recipe.where(public: true).includes(:user, :recipe_foods, recipe_foods: [:food])
   end
+end
